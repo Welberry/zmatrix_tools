507aade8641f31c6bb64e09742e88a434b24efae
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2008-07-04 05:08:26 +0000

    Added the ability to automatically determine 'location' and 'type' in the unit cell and write this information to the qxyz file. Now this file will function as a map file to convert between my mol2 based contact files and Darren's system which is based on z-matrices, locations and molecule types
    Removed the templating stuff temporarily.

--- zmat_maker.f90
+++ zmat_maker.f90
@@ -10,10 +10,15 @@ program zmat_maker
   use file_functions, only: stderr, stdout, log, delete
   use string_functions, only: join
   use iso_varying_string
+  use variable_array
 
   implicit none
 
   !! $Log: zmat_maker.f90,v $
+  !! Revision 1.3  2007/02/12 03:48:25  aidan
+  !! Big code clean up. Took the fitting routine out into a separate function.
+  !! Removed unused variables. Added command line arguments.
+  !!
   !! Revision 1.2  2006/05/18 06:05:17  aidan
   !! Working version that can automatically output multiple z-matrices.
   !!
@@ -25,7 +30,7 @@ program zmat_maker
   ! The 'ident' command can extract this version string from an
   ! object file or executable, which means one can identify which
   ! version of the module was used to compile it.
-  character(len=*), parameter :: version = "$Id: zmat_maker.f90,v 1.2 2006/05/18 06:05:17 aidan Exp aidan $"
+  character(len=*), parameter :: version = "$Id: zmat_maker.f90,v 1.3 2007/02/12 03:48:25 aidan Exp $"
 
   type (mol2_object)    :: mol2
   type (zmatrix_object) :: zmat(100), zmat_template
@@ -33,19 +38,26 @@ program zmat_maker
 
   type (quaternion)  :: q
 
-  real    :: rsd, trans(3), rsd_limit
-  integer :: i, j, iz, numzmat, error, ind
-  logical :: verbose, have_template
+  real    :: rsd, trans(3), rsd_limit, com(3), loc_coincident
+  integer :: i, j, k, iz, numzmat, numloc, error, ind, loc, moltype, dontcare
+  logical :: verbose, have_template, new_location
 
   character(len=1000)   :: fname, outname, buffer
   type (varying_string) :: myoptions(4)
+
+  type location
+     real :: coords(3)
+     integer, pointer :: zmats(:), number(:)
+  end type location
+
+  type(location), allocatable :: locations(:)
 
   ! These are our accepted command line options (see subroutine usage for
   ! an explanation)
   myoptions(1) = 'help'
   myoptions(2) = 'rsd'
   myoptions(3) = 'quiet'
-  myoptions(4) = 'template'
+  myoptions(4) = 'locationtol'
 
   ! This call parses the command line arguments for command line options
   call get_options(myoptions, error)
@@ -84,20 +96,33 @@ program zmat_maker
      rsd_limit = 0.01
   end if
 
-  ! See if we have specified a template z-matrix file
-  if (option_exists('template')) then
+  ! See if we have a tolerance for deciding if molecules are on the same location
+  if (option_exists('locationtol')) then
      ! Make sure we have a value
-     if (.NOT. has_value('template')) then
-        write(stderr,*) 'Must specify a filename with the option template!'
+     if (.NOT. has_value('locationtol')) then
+        write(stderr,*) 'Option locationtol must have a value!'
         call usage
         stop
      end if
-     fname = get_value('template')
-     zmat_template = fname
-     have_template = .TRUE.
+     loc_coincident = get_value('locationtol')
   else
-     have_template = .FALSE.
+     loc_coincident = 4.
   end if
+
+!!$  ! See if we have specified a template z-matrix file
+!!$  if (option_exists('template')) then
+!!$     ! Make sure we have a value
+!!$     if (.NOT. has_value('template')) then
+!!$        write(stderr,*) 'Must specify a filename with the option template!'
+!!$        call usage
+!!$        stop
+!!$     end if
+!!$     fname = get_value('template')
+!!$     zmat_template = fname
+!!$     have_template = .TRUE.
+!!$  else
+!!$     have_template = .FALSE.
+!!$  end if
 
   ! Grab the mol2 file name from the command line
   fname = next_arg()
@@ -122,6 +147,9 @@ program zmat_maker
   call delete(trim(outname),error)
      
   numzmat = 0
+  numloc = 0
+
+  allocate(locations(sub_num(mol2)))
 
   ! For each substructure we attempt to find a quaternion
   ! and associated centre of mass translations that will
@@ -136,11 +164,19 @@ program zmat_maker
      ! make a new one!
      do while (rsd > rsd_limit)
         iz = iz + 1
+        if (iz > sub_num(mol2)) then
+           write(stdout,'(A,I0)') 'WOAH! Something nasty has happened .. have failed to fit substructure ',i
+           write(stdout,'(A)') 'below the rsd limit. There is probably something wrong with the z-matrix'
+           write(stdout,'(A)') 'this program will produce. Examine it for any coincident atoms, reorder '
+           write(stdout,'(A)') 'the definition for that atom and re-run.'
+           ! write(stdout,'(A)') 'the definition for that atom and use this z-matrix as a template and re-run.'
+           exit
+        end if
         if (iz > numzmat) then
            ! Make a new z-matrix, as none of the other ones are a good fit
            numzmat = numzmat + 1
            if (have_template) then
-              zmat(numzmat) = as_zmatrix(zmat_template,mol2,i)
+              ! zmat(numzmat) = as_zmatrix(zmat_template,mol2,i)
            else
               zmat(numzmat) = as_zmatrix(mol2,i)
            end if
@@ -148,7 +184,39 @@ program zmat_maker
         rsd = fit(zmat(iz), coords(mol2,i), order(mol2,i), q, trans)
      end do
 
-     write(buffer,'(2I6, 4F10.6,L,3F11.6)') i, iz, as_array(q), improper(q), trans
+     com = sum(coords(mol2,i),dim=2)/real(atom_num(mol2,i))
+     new_location = .true.
+
+     NUMLOOP: do j = 1, numloc
+        if (sum(abs(com - locations(j)%coords)) < loc_coincident) then
+           ! These molecules are in the same place.
+           loc = j
+           new_location = .false.
+           do k = 1, size(locations(j)%zmats)
+              if (locations(j)%zmats(k) == iz) then
+                 locations(j)%number(k) = locations(j)%number(k) + 1
+                 moltype = locations(j)%number(k) 
+                 exit NUMLOOP
+              end if
+           end do
+           ! If we got here then there were no zmatrices of this type
+           ! at this location. So we put a new zmat on this location, 
+           ! with population 1
+           dontcare = push(locations(j)%zmats,iz)
+           dontcare = push(locations(j)%number,1)
+           moltype = 1
+        end if
+     end do NUMLOOP
+
+     if (new_location) then
+        numloc = numloc + 1
+        if (push(locations(numloc)%zmats,iz) /= 1) stop 'Error initialising location!'
+        moltype = push(locations(numloc)%number,1)
+        locations(numloc)%coords = com
+        loc = numloc
+     end if
+
+     write(buffer,'(4I6, 4F10.6,L,3F11.6)') i, loc, iz, moltype, as_array(q), improper(q), trans
      call log(outname, trim(buffer))
 
      if (verbose) then
@@ -235,12 +303,13 @@ contains
     write(stderr,*) 'is output for each residue. A new z-matrix is required when' 
     write(stderr,*) 'a fit exceeds the root square deviation (RSD) limit.'
     write(stderr,*)
-    write(stderr,*) 'Usage: zmat_maker [--help] [--quiet] [--rsd=value] [--template=zmatrix_file] mol2file'
+    write(stderr,*) 'Usage: zmat_maker [--help] [--quiet] [--rsd=value] mol2file'
+    ! write(stderr,*) 'Usage: zmat_maker [--help] [--quiet] [--rsd=value] [--template=zmatrix_file] mol2file'
     write(stderr,*)
     write(stderr,*) '  --help     - print this message'
     write(stderr,*) '  --quiet    - less verbose output'
     write(stderr,*) '  --rsd      - the value at which a new z-matrix is created'
-    write(stderr,*) '  --template - specify a template z-matrix file'
+    ! write(stderr,*) '  --template - specify a template z-matrix file'
     write(stderr,*)
 
   end subroutine usage
